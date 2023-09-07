36c526e92c39e136450cae8888b96e49cbced9c3
Anh Vu <vuna2004@gmail.com>
2023-09-07 16:07:58 +0700

    [ADD] Release Odoo Clarity 16.0.0.1

---
+++ odoo_clarity/README.md
@@ -0,0 +1,35 @@
+# Odoo Clarity
+
+## Add a Project
+
+1. Sign in to your [Clarity](https://clarity.microsoft.com/) account.
+
+2. Select 'New project' to start adding a project.
+![add-project-step-1](./static/description/add-project-step-1.png)
+
+3. Enter the Name and Website details. Select Add new project once done.
+![add-project-step-2](./static/description/add-project-step-2.png)
+
+## Install tracking code
+
+1. Select a project and go to Settings -> Setup
+![setup-step-1](./static/description/setup-step-1.png)
+
+2. Choose get Tracking Code
+![setup-step-2](./static/description/setup-step-2.png)
+
+3. copy code to the xml file (odoo_clarity/views/clarity_views.xml)
+
+    ![setup-step-3](./static/description/setup-step-3.png)
+
+    ![setup-step-4](./static/description/setup-step-4.png)
+
+## Credits
+
+Authors
+
+* Vu Nguyen
+
+Contributors
+
+* Vu Nguyen <vuna2004@gmail.com>
---
+++ odoo_clarity/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Odoo Clarity",
+    "summary": "Odoo Clarity: is a free, easy-to-use tool that captures how real people actually use your site. Setup is easy and you'll start getting data in minutes.",
+    "version": "16.0.0.1",
+    "category": "Extra tools",
+    "license": "LGPL-3",
+    "author": "Vu Nguyen",
+    "website": "",
+    "price": 0,
+    "sequence": 10,
+    "contributors": [
+    ],
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'views/clarity_views.xml',
+    ],
+    "qweb": [
+    ],
+    "images": [
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "assets": {
+        "web.assets_backend": [
+        ],
+        "web.assets_qweb": [
+        ],
+    },
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "images": ['static/description/banner.png'],
+}
